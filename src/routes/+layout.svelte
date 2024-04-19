d1a5a7e54ced57a7de219b8601f355bb91f26e4d
Nicolas Rio <nicolas.rio42@gmail.com>
2024-04-19 18:20:46 +0200

    WIP

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,15 +7,33 @@
 </script>
 
 <div flex="~ col" h-full>
-	<nav shadow-xl>
+	<nav shadow-xl class="container-fluid" pl-0>
 		<ul>
 			<li py-2>
 				<a href="/" flex items-center gap-2 py-0>
-					<div bg-primary w-10 h-10 flex items-center justify-center>
+					<div bg-primary w-11 h-11 flex items-center justify-center>
 						<i i-twemoji-ant block w6 h6 text-background-color class="-scale-x-100"></i>
 					</div>
 
 					Mapant.fr
+				</a>
+			</li>
+		</ul>
+
+		<ul>
+			<li py-0 flex>
+				<a href="" flex items-center gap-2 py-0>
+					<i i-carbon-information block w5 h5></i>
+
+					Infos
+				</a>
+			</li>
+
+			<li py-0 pr-0>
+				<a href="" role="button" class="!flex !py-1" items-center gap-2 m-0>
+					<i i-carbon-crop-growth block w5 h5></i>
+
+					Contribuer
 				</a>
 			</li>
 		</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,65 @@
-<script>
+<script lang="ts">
 	import LidarHdTiles from '$lib/components/map/LidarHdTiles.svelte';
 	// import MapLibreLayer from '$lib/components/map/MapLibreLayer.svelte';
 	import Mapant from '$lib/components/map/Mapant.svelte';
 	import OLMap from '$lib/components/map/OLMap.svelte';
 	import Osm from '$lib/components/map/OSM.svelte';
+	import { onMount } from 'svelte';
+
+	let isWelcomeDialogOpen = false;
+
+	onMount(() => setTimeout(() => (isWelcomeDialogOpen = true), 3000));
 </script>
+
+{#if isWelcomeDialogOpen}
+	<dialog open>
+		<article>
+			<div flex>
+				<h2 grow>Bienvenu sur Mapant.fr !</h2>
+
+				<button type="button" p-2 class="outline" on:click={() => (isWelcomeDialogOpen = false)}>
+					<i i-carbon-close-large w-5 h-5 block></i>
+				</button>
+			</div>
+
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro adipisci, repellendus dolor
+				veritatis labore numquam doloribus beatae magnam ullam officia iusto accusantium architecto
+				obcaecati expedita consequatur dicta asperiores nihil rem.
+			</p>
+
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro adipisci, repellendus dolor
+				veritatis labore numquam doloribus beatae magnam ullam officia iusto accusantium architecto
+				obcaecati expedita consequatur dicta asperiores nihil rem.
+			</p>
+
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro adipisci, repellendus dolor
+				veritatis labore numquam doloribus beatae magnam ullam officia iusto accusantium architecto
+				obcaecati expedita consequatur dicta asperiores nihil rem.
+			</p>
+
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro adipisci, repellendus dolor
+				veritatis labore numquam doloribus beatae magnam ullam officia iusto accusantium architecto
+				obcaecati expedita consequatur dicta asperiores nihil rem.
+			</p>
+
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro adipisci, repellendus dolor
+				veritatis labore numquam doloribus beatae magnam ullam officia iusto accusantium architecto
+				obcaecati expedita consequatur dicta asperiores nihil rem.
+			</p>
+
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro adipisci, repellendus dolor
+				veritatis labore numquam doloribus beatae magnam ullam officia iusto accusantium architecto
+				obcaecati expedita consequatur dicta asperiores nihil rem.
+			</p>
+		</article>
+	</dialog>
+{/if}
 
 <main grow>
 	<OLMap>
