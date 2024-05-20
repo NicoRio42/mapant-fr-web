995bf1ef7b32f3c75a24ae3b99804938dba54fc4
Nicolas Rio <nicolas.rio42@gmail.com>
2024-05-20 18:27:18 +0200

    Better pricing section

--- src/routes/contribute/step-2/+page.svelte
+++ src/routes/contribute/step-2/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Formula from './Formula.svelte';
+
 	import Stepper from '../Stepper.svelte';
 </script>
 
@@ -6,42 +8,40 @@
 
 <main px-4>
 	<p max-w-200 m-x-auto>
-		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius voluptate et fuga doloribus
-		repudiandae facilis velit consectetur porro minus alias magni delectus assumenda, tempore sunt
-		accusantium repellendus facere consequatur atque!
+		En contrepartie de votre contribution, vous pouvez sélectionner une zone sur la carte de France.
+		Elle sera traitée en priorité lors de la génération de la carte à partir des données LiDAR.
 	</p>
 
 	<div grid="~ md:cols-2 lg:cols-4" gap-8>
-		<article>
-			<header>
-				<h2>L'amateur de carte</h2>
-			</header>
+		<Formula title="Format A4" price={10} linkText="Choisir le format A4" formulaId="1">
+			La surface équivalente à une carte au format A4 à l'échelle 1:10000
+		</Formula>
 
-			<a role="button" href="/contribute/step-3?formula=1">Choisir le format A3</a>
-		</article>
+		<Formula title="Format A3" price={20} linkText="Choisir le format A3" formulaId="2">
+			La surface équivalente à une carte au format A3 à l'échelle 1:10000
+		</Formula>
 
-		<article>
-			<header>
-				<h2>Le geek de la CO</h2>
-			</header>
+		<Formula title="Format A1" price={50} linkText="Choisir le format A1" formulaId="3">
+			La surface équivalente à une carte au format A1 à l'échelle 1:10000
+		</Formula>
 
-			<a role="button" href="/contribute/step-3?formula=2">Choisir le format A1</a>
-		</article>
+		<Formula
+			title="Format double A0"
+			price={100}
+			linkText="Choisir le format double A0"
+			formulaId="4"
+		>
+			La surface équivalente à une carte au format double A0 à l'échelle 1:10000
+		</Formula>
 
-		<article>
-			<header>
-				<h2>Le cartographe</h2>
-			</header>
-
-			<a role="button" href="/contribute/step-3?formula=3">Choisir le format A-1</a>
-		</article>
-
-		<article>
-			<header>
-				<h2>Le mécène</h2>
-			</header>
-
-			<a role="button" href="/contribute/step-3?formula=4">Choisir une dalle entière</a>
-		</article>
+		<Formula
+			title="Format méga tuile"
+			price={500}
+			linkText="Choisir le format A4"
+			formulaId="5"
+			colspan={2}
+		>
+			Une méga tuile de 50km par 50km
+		</Formula>
 	</div>
 </main>
---
+++ src/routes/contribute/step-2/Formula.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let title: string;
+	export let price: number;
+	export let linkText: string;
+	export let formulaId: string;
+	export let colspan = 1;
+</script>
+
+<article style:grid-column="span {colspan}">
+	<header flex items-center py-6>
+		<h2 grow mb-0>{title}</h2>
+
+		<p mb-0 text-8 font-700>{price} €</p>
+	</header>
+
+	<p>
+		<slot></slot>
+	</p>
+
+	<p text-right>
+		<a role="button" href="/contribute/step-3?formula={formulaId}">{linkText}</a>
+	</p>
+</article>
