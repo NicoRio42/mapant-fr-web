d7499d77beb14c8ecdb25c3bd9372e988de16462
Nicolas Rio <nicolas.rio42@gmail.com>
2024-05-23 15:51:08 +0200

    Debuging webhook

--- src/routes/api/stripe/webhook/+server.ts
+++ src/routes/api/stripe/webhook/+server.ts
@@ -26,24 +26,24 @@ export async function POST({ request }) {
 		return new Response(null, { status: 400 });
 	}
 
-	setTimeout(async () => {
-		if (event.type !== 'checkout.session.completed') return;
+	if (event.type !== 'checkout.session.completed') return;
 
-		if (event.data.object.client_reference_id === null) {
-			console.error('client_reference_id not defined.');
-			return;
-		}
+	if (event.data.object.client_reference_id === null) {
+		console.error('[STRIPE_WEBHOOK] client_reference_id not defined.');
+		return new Response(null, { status: 400 });
+	}
 
-		const [contribution] = await db
-			.update(contributionTable)
-			.set({ paied: true })
-			.where(eq(contributionTable.id, event.data.object.client_reference_id))
-			.returning();
+	console.log('client_reference_id', event.data.object.client_reference_id);
 
-		if (contribution === undefined) {
-			console.error('[STRIPE_WEBHOOK] No contribution matches the given client_reference_id.');
-		}
-	});
+	const [contribution] = await db
+		.update(contributionTable)
+		.set({ paied: true })
+		.where(eq(contributionTable.id, event.data.object.client_reference_id))
+		.returning();
+
+	if (contribution === undefined) {
+		console.error('[STRIPE_WEBHOOK] No contribution matches the given client_reference_id.');
+	}
 
 	return new Response(null, { status: 200 });
 }
