d9e3345052fa27a8a89263a5003896bae02ac61f
Nicolas Rio <nicolas.rio42@gmail.com>
2024-06-28 17:43:03 +0200

    Metatags

--- src/routes/blog/karttapullautin-pour-les-nuls/+page.svelte
+++ src/routes/blog/karttapullautin-pour-les-nuls/+page.svelte
@@ -1,6 +1,28 @@
 <script>
+	import { MetaTags } from 'svelte-meta-tags';
 	import content from './karttapullautin-pour-les-nuls.md';
+	import { WEBSITE_NAME } from '$lib/constants';
+	import { page } from '$app/stores';
 </script>
+
+<MetaTags
+	title="{WEBSITE_NAME} | Blog | Karttapullautin pour les nuls"
+	description="Tutoriel pour générer une carte de course d'orientation avec Karttapullautin"
+	canonical={new URL($page.url.pathname, $page.url.origin).href}
+	openGraph={{
+		url: new URL($page.url.pathname, $page.url.origin).href,
+		locale: 'fr_FR',
+		images: [
+			{
+				url: '/karttapullautin-exemple.jpg',
+				width: 1107,
+				height: 499,
+				alt: 'A LiDAR generated orienteering map with karttapullautin'
+			}
+		]
+	}}
+	twitter={{ cardType: 'summary_large_image' }}
+/>
 
 <main container max-w-200 m-x-auto py-8 px-4>
 	<h1>Karttapullautin pour les nuls</h1>
