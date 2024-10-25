2fba811a15ebe3affb7e070cbd0dbc2bc1d04308
Nicolas Rio <nicolas.rio42@gmail.com>
2024-10-25 12:34:11 +0200

    Better post layout

--- src/routes/blog/[...slug]/+page.svelte
+++ src/routes/blog/[...slug]/+page.svelte
@@ -4,6 +4,8 @@
 	import { page } from '$app/stores';
 
 	let { data } = $props();
+
+	const dateFormater = new Intl.DateTimeFormat('fr', { dateStyle: 'short' });
 </script>
 
 <MetaTags
@@ -18,21 +20,38 @@
 	twitter={{ cardType: 'summary_large_image' }}
 />
 
-<main container max-w-200 m-x-auto py-8 px-4>
-	<h1
-		style:--view-transition-name="title-{data.post.slug}"
-		class="[view-transition-name:var(--view-transition-name)]"
-	>
-		{data.post.frontmatter.title}
-	</h1>
-
+<div relative>
 	<img
-		style:--view-transition-name="banner-{data.post.slug}"
-		class="[view-transition-name:var(--view-transition-name)]"
 		src={data.post.frontmatter.banner}
 		alt=""
-		my-4
+		decoding="async"
+		w-full
+		h-75
+		object-cover
+		style:--view-transition-name="title-{data.post.slug}"
+		class="[view-transition-name:var(--view-transition-name)]"
 	/>
 
+	<div absolute right-0 bottom-0 left-0 p="y-4 x-6" bg-gray-800 bg-op-60>
+		<h1
+			text="10 white"
+			mb-2
+			style:--view-transition-name="banner-{data.post.slug}"
+			class="[view-transition-name:var(--view-transition-name)]"
+		>
+			{data.post.frontmatter.title}
+		</h1>
+
+		<p text="white 4.5" mb-2>
+			{data.post.frontmatter.description}
+		</p>
+
+		<p text-white mb-2>
+			{data.post.frontmatter.author} - {dateFormater.format(new Date(data.post.frontmatter.date))}
+		</p>
+	</div>
+</div>
+
+<main container max-w-200 m-x-auto py-8 px-4>
 	{@html data.post.content}
 </main>
